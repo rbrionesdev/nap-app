431ca216ef41ce9a8aed14a3f05321d2fb3dfbe2
Lee Anderson <ecllips07@gmail.com>
2021-10-05 13:59:36 -0600

    edited controller

--- app/controllers/api/punchcards_controller.rb
+++ app/controllers/api/punchcards_controller.rb
@@ -4,7 +4,7 @@ class Api::PunchcardsController < ApplicationController
 
   def index
     punchcard = Punchcard.all_and_then_some
-    render json: appointments
+    render json: punchcards
   end
 
   def create
