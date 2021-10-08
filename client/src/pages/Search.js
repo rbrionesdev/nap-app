7ee5f6c91ba328c0a740e10969f04085878ee58b
Alan Mamulski <alanmamulski@gmail.com>
2021-10-07 22:44:23 -0600

    live search bar working

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,12 +3,13 @@ import axios from 'axios'
 import {Card} from 'semantic-ui-react'
 import { AuthContext } from '../providers/AuthProvider';
 import MyCard from '../Components/MyCard';
+import SearchBar from '../Components/SearchBar';
 
 
 const Search = () => {
-  const { user } = useContext(AuthContext)
+  const [input, setInput] = useState('');
   const [cards, setCards] = useState([])
-  const [ownedCards, setOwnedCards] = useState([])
+  const [searchCards, setSearchCards] = useState([])
 
   useEffect(()=>{
     getCards()
@@ -18,19 +19,32 @@ const Search = () => {
     try{
       let res = await axios.get('/api/punchcards/all')
       setCards(res.data)
+      setSearchCards(res.data)
       console.log('all cards',res.data)
     }catch(err){
       console.log(err)
     }
   }
+
+  const updateInput = async (input) => {
+    const filtered = cards.filter(c => {
+     return c.restaurant_name.toLowerCase().includes(input.toLowerCase())
+    })
+    setInput(input);
+    setSearchCards(filtered);
+ }
   
   const renderCards = () => {
-    return cards.map((c) => <MyCard {...c}/>)
+    return searchCards.map((c) => <MyCard {...c}/>)
   }
 
   return (
     <div>
       <h1>Search</h1>
+      <SearchBar
+        input={input} 
+        onChange={updateInput}
+       />
       {renderCards()}
     </div>
   )
