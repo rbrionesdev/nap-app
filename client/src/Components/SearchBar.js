d74826c972d72f9dec0823caf523b435746ee486
Alan Mamulski <alanmamulski@gmail.com>
2021-10-07 22:54:11 -0600

    style search bar

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Input } from 'semantic-ui-react';
 
 const SearchBar = ({input:keyword, onChange:setKeyword}) => {
   const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
   return (
-    <input 
+    <Input 
      style={BarStyling}
      key="random1"
      value={keyword}
