81920788de99e25319180cda391d44021d73b9db
Tom Hohl <tom.hohl@gmail.com>
2021-10-09 12:33:53 -0600

    started styling landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useHistory } from 'react-router';
 import { Button } from 'semantic-ui-react'
+import '../StyleSheets/LandingPage.css'
 
 const LandingPage = () => {
   const history = useHistory()
   return(
     <div>
-      <h1>Landing Page</h1>
-      <Button onClick={(e)=>history.push('/about')}>About the Creators</Button>
+      <div className='title-wrapper'>
+        <p className='the'>The</p>
+        <p className='nap'>Nap</p>
+        <p className='app'>App</p>
+      </div>
+
+      <div className='about'>
+        <Button onClick={(e)=>history.push('/about')}>About the Creators</Button>
+      </div>
     </div>
   )
 }
