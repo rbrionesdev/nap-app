bccf9f605c4e030cbea36812ff0727482d1f87a7
Tom Hohl <tom.hohl@gmail.com>
2021-10-09 15:19:37 -0600

    weekly added to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
-import { Button } from "react-bootstrap";
 import { Redirect } from 'react-router';
+import { Menu } from 'semantic-ui-react';
+import CalendarWeekly from '../Components/CalendarWeekly';
 import NapFeed from '../Components/NapFeed';
 import { AuthContext } from '../providers/AuthProvider';
 import '../StyleSheets/Home.css'
@@ -11,12 +12,21 @@ const Home = (props) => {
     return (
       <div className="page-wrapper">
         <div className="button-wrapper">
-          <Button variant='outline-secondary' onClick={() => props.history.push("/nap/new")}>Schedule Nap</Button>
-          <Button variant='outline-secondary' onClick={() => props.history.push("/naps")}>View All Scheduled Naps</Button>
+          <Menu vertical>
+            <Menu.Item onClick={() => props.history.push("/nap/new")}>
+              Schedule Nap Time
+            </Menu.Item>
+            <Menu.Item onClick={() => props.history.push("/naps")}>
+              View Scheduled Naps
+            </Menu.Item>
+          </Menu>
         </div>
         <div className='feed-wrapper'>
           <h2>Who's napping</h2>
           <NapFeed />
+        </div>
+        <div className="calendar-wrapper">
+          <CalendarWeekly />
         </div>
       </div>
     )
