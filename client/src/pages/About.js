225477eae6a32dd625d6671b9eff352cb2504929
Tom Hohl <tom.hohl@gmail.com>
2021-10-09 12:08:24 -0600

    about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Card, Image } from 'semantic-ui-react';
+import julieImg from '../imgs/julie_img.png'
+import tomImg from '../imgs/tom_img.png'
+import rafImg from '../imgs/raf_img.png'
 
 const About = () => {
   return(
     <div>
-      <h1>About Page</h1>
+      <h1>The creators</h1>
+      <Card.Group centered>
+        <Card>
+          <Image src={tomImg} wrapped ui={false}/>
+          <Card.Content>
+            <Card.Header>Tom</Card.Header>
+            <Card.Meta>Co-Creator</Card.Meta>
+            <Card.Description>
+              Tom used to live his life in theater, now lives on a computer
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src={julieImg} wrapped ui={false}/>
+          <Card.Content>
+            <Card.Header>Julie</Card.Header>
+            <Card.Meta>Co-Creator</Card.Meta>
+            <Card.Description>
+              Julie is a former chemist turned programmer
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src={rafImg} wrapped ui={false}/>
+          <Card.Content>
+            <Card.Header>Rafael</Card.Header>
+            <Card.Meta>Co-Creator</Card.Meta>
+            <Card.Description>
+              Sushi loving, Vegas regular Rafael has begun his programming career in Utah
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Card.Group>
     </div>
   )
 }
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useHistory } from 'react-router';
+import { Button } from 'semantic-ui-react'
 
 const LandingPage = () => {
+  const history = useHistory()
   return(
     <div>
       <h1>Landing Page</h1>
+      <Button onClick={(e)=>history.push('/about')}>About the Creators</Button>
     </div>
   )
 }
