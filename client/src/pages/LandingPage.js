687dbb6ebad1cf4c0293366b38390b64bdc262b1
Julie Tang <julieanntang@hotmail.com>
2021-10-09 15:24:33 -0600

    landing page styles

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { useHistory } from 'react-router';
 import { Button } from 'semantic-ui-react'
 import '../StyleSheets/LandingPage.css'
+import moon2 from '../imgs/moon2.png'
 
 const LandingPage = () => {
   const history = useHistory()
   return(
-    <div>
+    <div className='photo'
+      // backgroundImage: `url("https://www.pinclipart.com/picdir/big/416-4166926_sleep-logo-transparent-clipart.png")`
+    >
+      <div className='login'>
+      <Button onClick={(e)=>history.push('/login')}>Login</Button>
+      <Button onClick={(e)=>history.push('/register')}>Register</Button>
+      </div>
       <div className='title-wrapper'>
         <p className='the'>THE</p>
         <p className='nap'>NAP</p>
